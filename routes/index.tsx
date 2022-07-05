8a9b400e1ae8d18fbb5d82d689d1522aeb8d0054
Fuad Elhasan Irfani <73324192+feilvan@users.noreply.github.com>
2022-07-05 23:08:49 +0700

    rawr

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,21 +1,17 @@
 /** @jsx h */
 import { h } from "preact";
 import { tw } from "@twind";
-import Counter from "../islands/Counter.tsx";
 
 export default function Home() {
   return (
-    <div class={tw`p-4 mx-auto max-w-screen-md bg-red-300`}>
-      <img
-        src="/logo.svg"
-        height="100px"
-        alt="the fresh logo: a sliced lemon dripping with juice"
-      />
-      <p class={tw`my-6`}>
-        Welcome to `fresh`. Try update this message in the ./routes/index.tsx
-        file, and refresh.
-      </p>
-      <Counter start={3} />
+    <div class={tw`flex flex-1 flex-col p-4 w-full h-screen my-0 mx-auto bg-black text-white font-mono`}>
+      <div class={tw`flex flex-col flex-1 justify-center items-center`}>
+        <img src="/images/dino.svg" alt="Chromium T-rex"/>
+	      <span class=" font-bold pt-8">rraawwrr!</span>
+      </div>
+      <div class={tw`flex flex-col justify-center items-center`}>
+        <span>©{new Date().getFullYear()} truclr</span>
+      </div>
     </div>
   );
 }
